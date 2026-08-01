4a192c8db59255b632696edb4b82765b62322085
shellatol-commits <lucas.necel@gmail.com>
2026-08-01 16:19:08 +0100

    Add online detection extension
    This extension detects internet connectivity status.

---
+++ exts/online.js
@@ -0,0 +1,98 @@
+/*
+   Created with ExtForge
+   https://jwklong.github.io/extforge
+*/
+(async function(Scratch) {
+    const variables = {};
+
+
+    if (!Scratch.extensions.unsandboxed) {
+        alert("This extension needs to be unsandboxed to run!")
+        return
+    }
+
+    const ExtForge = {
+        Broadcasts: new function() {
+            this.raw_ = {};
+            this.register = (name, blocks) => {
+                this.raw_[name] = blocks;
+            };
+            this.execute = async (name) => {
+                if (this.raw_[name]) {
+                    await this.raw_[name]();
+                };
+            };
+        },
+
+        Variables: new function() {
+            this.raw_ = {};
+            this.set = (name, value) => {
+                this.raw_[name] = value;
+            };
+            this.get = (name) => {
+                return this.raw_[name] ?? null;
+            }
+        },
+
+        Vector: class {
+            constructor(x, y) {
+                this.x = x;
+                this.y = y;
+            }
+
+            static from(v) {
+                if (v instanceof ExtForge.Vector) return v
+                if (v instanceof Array) return new ExtForge.Vector(Number(v[0]), Number(v[1]))
+                if (v instanceof Object) return new ExtForge.Vector(Number(v.x), Number(v.y))
+                return new ExtForge.Vector()
+            }
+
+            add(v) {
+                return new Vector(this.x + v.x, this.y + v.y);
+            }
+
+            set(x, y) {
+                return new Vector(x ?? this.x, y ?? this.y)
+            }
+        },
+
+        Utils: {
+            setList: (list, index, value) => {
+                [...list][index] = value;
+                return list;
+            },
+            lists_foreach: {
+                index: [0],
+                value: [null],
+                depth: 0
+            },
+            countString: (x, y) => {
+                return y.length == 0 ? 0 : x.split(y).length - 1
+            }
+        }
+    }
+
+    class Extension {
+        getInfo() {
+            return {
+                "id": "online",
+                "name": "Internet Detection",
+                "color1": "#0f72bd",
+                "blocks": [{
+                    "opcode": "block_e149adcd2fa134b5",
+                    "text": "online?",
+                    "blockType": "Boolean",
+                    "arguments": {}
+                }]
+            }
+        }
+        async block_e149adcd2fa134b5(args) {
+            return (eval(("window.navigator.onLine")))
+        }
+    }
+
+    let extension = new Extension();
+    // code compiled from extforge
+
+    Scratch.extensions.register(extension);
+})(Scratch);
